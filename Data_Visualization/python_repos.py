f60cca836ba79b808ef6e2f6a2d5d37eaf6cc200
Rahul-Rattan <emprahulrattan@gmail.com>
2024-11-16 23:59:48 -0500

    APIs

---
+++ Data_Visualization/python_repos.py
@@ -0,0 +1,12 @@
+import requests
+
+#Make an API call and check the response
+url="https://api.github.com/search/repositories?q=language:python+sort:stars+stars:>10000"
+header={"Accept":"application/vnd.github.v3+json"}
+r=requests.get(url,headers=header)
+print(f"Status code: {r.status_code}")
+
+#Convert the response object to a dictionary
+response_dict=r.json()
+
+#Process results
