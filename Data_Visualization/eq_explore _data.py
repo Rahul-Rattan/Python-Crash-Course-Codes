aed4584b67ed3f39a3c84dec276768a0d61c5cf3
Rahul-Rattan <emprahulrattan@gmail.com>
2024-11-16 22:04:24 -0500

    Visualization with plotly.express

--- Data_Visualization/eq_explore _data.py
+++ Data_Visualization/eq_explore _data.py
@@ -1,4 +1,32 @@
-import matplotlib.pyplot as pltS
-import csv
+import json
+from pathlib import Path
+import plotly.express as px
 
-fig, ax = plt.subplots()
+path=Path("C:/Users/rahul/OneDrive/Desktop/Files/python_works/Data_Visualization/eq_data_1_day_m1.geojson")
+lines=path.read_text("utf-8")
+all_eq_data=json.loads(lines)
+  
+all_eq_dicts = all_eq_data['features']
+print(len(all_eq_dicts))
+
+mags, lons, lats, eq_titles=[],[],[],[]
+for eq_dict in all_eq_dicts:
+    mag=eq_dict['properties']['mag']
+    lon=eq_dict['geometry']['coordinates'][0]
+    lat=eq_dict['geometry']['coordinates'][1]
+    eq_title = eq_dict['properties']['title']
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+    eq_titles.append(eq_title)
+
+
+print(mags[:10])
+print(lons[:5])
+print(lats[:5])
+
+title="Global Earthquakes"
+fig = px.scatter_geo(lat=lats, lon=lons, size=mags, title=title,
+color=mags,color_continuous_scale='Viridis',labels={'color':'Magnitude'},projection='natural earth',hover_name=eq_titles,)
+fig.show()
+
